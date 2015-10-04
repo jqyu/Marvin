772beba9be82384405c067584f032f06aea19367
James Yu <jj6yu@uwaterloo.ca>
2015-10-04 11:31:40 -0400

    re-hello re-world

---
+++ app/src/main/java/whatever/marvin/MainActivity.java
@@ -0,0 +1,31 @@
+package whatever.marvin;
+
+import android.content.Context;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    Vibrator v;
+    long[] vPattern = {0, 1000, 200};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hides title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Hides notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        // vibrate !!!
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(vPattern, 0);
+    }
+}
