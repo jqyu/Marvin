5016ab48252bad181f5b73f5f1009a8fbc346cbe
agentflare <samuel.wu.u1@gmail.com>
2015-10-04 17:44:35 -0400

    Marvin makes sounds
    yay

--- app/src/main/java/whatever/marvin/MainActivity.java
+++ app/src/main/java/whatever/marvin/MainActivity.java
@@ -2,12 +2,16 @@ package whatever.marvin;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.net.Uri;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.Window;
 import android.view.WindowManager;
+
+import java.util.Random;
 
 public class MainActivity extends Activity implements CameraFragment.OnFragmentInteractionListener {
 
@@ -15,7 +19,7 @@ public class MainActivity extends Activity implements CameraFragment.OnFragmentI
     long[] vPattern = {0, 1000, 200};
 
     MarvinFragment marvin = null;
-
+    MediaPlayer mediaPlayer;
     public void panic(){
 
         // vibrate !!!
@@ -26,7 +30,56 @@ public class MainActivity extends Activity implements CameraFragment.OnFragmentI
         if (marvin != null) {
             marvin.panic();
         }
-
+        Uri uri = null;
+        if(mediaPlayer!=null) {
+            if (!mediaPlayer.isPlaying()) {
+                Random gen = new Random();
+                int i = gen.nextInt(5);
+                Log.i("MainActivity",String.valueOf(i));
+                switch (i) {
+                    case 0:
+                        mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_02);
+                        break;
+                    case 1:
+                        mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_04);
+                        break;
+                    case 2:
+                        mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_05);
+                        break;
+                    case 3:
+                        mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_01);
+                        break;
+                    case 4:
+                        mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_03);
+                        break;
+                }
+                mediaPlayer.start();
+            }
+        }
+        else
+        {
+            Random gen = new Random();
+            int i = gen.nextInt(5);
+            Log.i("MainActivity",String.valueOf(i));
+            switch (i) {
+                case 0:
+                    mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_02);
+                    break;
+                case 1:
+                    mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_04);
+                    break;
+                case 2:
+                    mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_05);
+                    break;
+                case 3:
+                    mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_01);
+                    break;
+                case 4:
+                    mediaPlayer = MediaPlayer.create(this, R.raw.robonoise_03);
+                    break;
+            }
+            mediaPlayer.start();
+        }
     }
 
     @Override
